2033b615c0ac383d7d446e35f9b23a5a77278662
babyplug <poramin.le.58@ubu.ac.th>
2020-10-11 19:02:10 +0700

    Bootstraping -- bootstrap project

---
+++ src/test/java/com/babyplug/softwaretesting/SoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyplug.softwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
