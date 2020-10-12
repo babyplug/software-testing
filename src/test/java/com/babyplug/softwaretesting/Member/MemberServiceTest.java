1f9714dda46d83bd9373c756b9fbb54b07f1d367
babyplug <poramin.le.58@ubu.ac.th>
2020-10-12 14:05:08 +0700

    MemberServiceTest -- change to run with @InjectMocks cause need no want constructor

--- src/test/java/com/babyplug/softwaretesting/Member/MemberServiceTest.java
+++ src/test/java/com/babyplug/softwaretesting/Member/MemberServiceTest.java
@@ -7,8 +7,12 @@ import com.babyplug.softwaretesting.member.service.MemberServiceImpl;
 import com.babyplug.softwaretesting.utils.PhoneNumberValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -19,27 +23,51 @@ import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 
+//@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
+
     @Mock
     private PhoneNumberValidator phoneNumberValidator;
 
-    private MemberService memberService;
+    @InjectMocks
+    private final MemberService memberService = new MemberServiceImpl();
 
-    @BeforeEach
-    void setup() {
-        MockitoAnnotations.openMocks(this);
-        memberService = new MemberServiceImpl(memberRepository, phoneNumberValidator);
+    @Captor
+    private ArgumentCaptor<Member> memberArgumentCaptor;
+
+//    @BeforeEach
+//    void setup() {
+//        MockitoAnnotations.openMocks(this);
+//        // memberService = new MemberServiceImpl(memberRepository, phoneNumberValidator);
+//    }
+
+    @Test
+    public void itShouldCreateMember() {
+        // given
+        Member member = new Member(1L,"a", 13L, "1234567890");
+
+        // valid phone
+        given(phoneNumberValidator.test(member.getTelNo())).willReturn(true);
+
+        // When
+        memberService.createMember(member);
+
+        // Then
+        then(memberRepository).should().save(memberArgumentCaptor.capture());
+        Member memberArgumentCaptorValue = memberArgumentCaptor.getValue();
+        assertThat(memberArgumentCaptorValue).isEqualTo(member);
     }
 
     @Test
-    void itShouldCreateMember() {
+    public void itShouldCreateMemberOld() {
         // given
-        doAnswer(returnsFirstArg()).when(memberRepository).save(any(Member.class));
+         doAnswer(returnsFirstArg()).when(memberRepository).save(any(Member.class));
 
-        // request
+        // given
         Member member = new Member(1L,"a", 13L, "1234567890");
 
         // valid phone
@@ -49,16 +77,14 @@ public class MemberServiceTest {
         Member memberResponse = memberService.createMember(member);
 
         // Then
+        then(memberRepository).should().save(any(Member.class));
         assertThat(memberResponse).isEqualTo(member);
-        verify(memberRepository).save(any(Member.class));
+//         verify(memberRepository).save(any(Member.class));
     }
 
     @Test
-    void itShouldNotCreateMemberWhenPhoneNumberIsInvalid() {
+    public void itShouldNotCreateMemberWhenPhoneNumberIsInvalid() {
         // given
-        doAnswer(returnsFirstArg()).when(memberRepository).save(any(Member.class));
-
-        // request
         Member member = new Member(1L,"a", 13L, "1234567890");
 
         // valid phone
@@ -71,7 +97,6 @@ public class MemberServiceTest {
 
         // Then
         then(memberRepository).shouldHaveNoInteractions();
-
     }
 
 }
